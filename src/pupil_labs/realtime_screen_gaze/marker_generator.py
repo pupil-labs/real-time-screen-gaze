7591490eb6c1a86c87cc023a83d7e339c9828230
Dominic Canare <dom@greenlightgo.org>
2023-05-15 07:42:30 -0500

    Adds explicit image flipping options

--- src/pupil_labs/realtime_screen_gaze/marker_generator.py
+++ src/pupil_labs/realtime_screen_gaze/marker_generator.py
@@ -2,6 +2,18 @@ import cv2
 
 apriltag_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_36h11)
 
-def generate_marker(marker_id, side_pixels=8):
-	flipped = apriltag_dict.generateImageMarker(marker_id, side_pixels, 0)
-	return cv2.rotate(flipped, cv2.ROTATE_180)
+def generate_marker(marker_id, side_pixels=8, flip_x=False, flip_y=False):
+	image_data = apriltag_dict.generateImageMarker(marker_id, side_pixels, 0)
+
+	flip_code = None
+	if flip_x and not flip_y:
+		flip_code = 1
+	elif not flip_x and flip_y:
+		flip_code = 0
+	elif flip_x and flip_y:
+		flip_code = -1
+
+	if flip_code is not None:
+		image_data = cv2.flip(image_data, flip_code)
+
+	return image_data
